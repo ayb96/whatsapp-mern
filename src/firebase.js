62d47b70e6920159118240385d3aeb16bbae2108
Hussein Ayoub <Hussein-Ayoub-96@hotmail.com>
2021-07-02 23:32:31 +0300

    create our whatsapp-app

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCfNafFEY0yL-XasD8nFoLCSUaMWfKFAO8",
+    authDomain: "whatsapp-a915a.firebaseapp.com",
+    projectId: "whatsapp-a915a",
+    storageBucket: "whatsapp-a915a.appspot.com",
+    messagingSenderId: "224579313518",
+    appId: "1:224579313518:web:8801dc41357192e81e2b7f"
+  };
